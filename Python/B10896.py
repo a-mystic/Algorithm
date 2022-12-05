80455df60d76b7ed124316c6e958f7362b9b8f05
a-mystic <totocaca@naver.com>
2022-12-05 17:24:35 +0900

    .

---
+++ Python/B10896.py
@@ -0,0 +1,8 @@
+numbers = input().split(' ')
+numbers = [int(numbers[0]), int(numbers[1])]
+
+print(numbers[0]+numbers[1])
+print(numbers[0]-numbers[1])
+print(numbers[0]*numbers[1])
+print(numbers[0]//numbers[1])
+print(numbers[0]%numbers[1])
