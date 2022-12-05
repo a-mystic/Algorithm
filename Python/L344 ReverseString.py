1edb3428e6ecb128d972bcd3728a1a7a5c6c4f7c
a-mystic <totocaca@naver.com>
2022-12-05 21:35:31 +0900

    .

---
+++ Python/L344 ReverseString.py
@@ -0,0 +1,12 @@
+# i) two pointer
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+
+# ii) pythonic
+    def reverseString(self, s: list[str]) -> None:
+        s.reverse()
