a93d8f8ab56a8c9740daf22028415c5c71529509
a-mystic <totocaca@naver.com>
2024-03-08 13:25:19 +0900

    add swift algorithm

---
+++ SwiftAlgorithm/SwiftAlgorithm/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  SwiftAlgorithm
+//
+//  Created by a mystic on 3/8/24.
+//
+
+import Foundation
+
+private func stackPractice() {
+//    var stack = Stack<Int>()
+//    stack.push(5)
+//    stack.push(44)
+//    for i in 0..<3 {
+//        if let value = stack.pop() {
+//            print(value)
+//        }
+//    }
+}
+
+stackPractice()
